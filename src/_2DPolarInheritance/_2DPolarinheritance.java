5c09a78c7f8689b32f67cdb58a2afb8d49de4741
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-11-27 21:36:51 +0100

    constructor added

--- src/_2DPolarInheritance/_2DPolarinheritance.java
+++ src/_2DPolarInheritance/_2DPolarinheritance.java
@@ -4,6 +4,11 @@ import Vector2D.Vector2D;
 
 public class _2DPolarinheritance extends Vector2D
 {
+    public _2DPolarinheritance(double x, double y)
+    {
+        super(x, y);
+    }
+
     public double getAngle()
     {
         return Math.atan(getComponents().get(1) / getComponents().get(0));
