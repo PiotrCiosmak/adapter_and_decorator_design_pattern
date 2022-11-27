6dbe1d8330266415ef44890c22fcf3c8b02852a1
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-11-27 20:57:16 +0100

    done

--- src/Vector3DInheritance/Vector3DInheritance.java
+++ src/Vector3DInheritance/Vector3DInheritance.java
@@ -7,37 +7,47 @@ import java.util.ArrayList;
 
 public class Vector3DInheritance extends Vector2D
 {
+    public Vector3DInheritance(double x, double y, double z)
+    {
+        super(x, y);
+        this.z = z;
+    }
+
     @Override
     public double abs()
     {
-        //TODO
-        return 0;
+        double x = getComponents().get(0);
+        double y = getComponents().get(1);
+        return Math.sqrt(x * x + y * y + z * z);
     }
 
     @Override
     public double cdot(IVector param)
     {
-        //TODO
-        return 0;
+        return getComponents().get(0) * param.getComponents().get(0) + getComponents().get(1) * param.getComponents().get(1) + z * 0;
     }
 
     @Override
     public ArrayList<Double> getComponents()
     {
-        //TODO
-        return null;
+        ArrayList<Double> arrayList = new ArrayList<>();
+        arrayList.add(getComponents().get(0));
+        arrayList.add(getComponents().get(1));
+        arrayList.add(z);
+        return arrayList;
     }
 
-    public Vector3D(IVector param)
+    public Vector3DInheritance cross(IVector param)
     {
-        //TODO
-        return null;
+        double newX = getComponents().get(1) * 0 - z * param.getComponents().get(1);
+        double newY = -(getComponents().get(0) * 0 - z * param.getComponents().get(0));
+        double newZ = getComponents().get(0) * param.getComponents().get(1) - getComponents().get(1) * param.getComponents().get(0);
+        return new Vector3DInheritance(newX, newY, newZ);
     }
 
     public IVector getSrcV()
     {
-        //TODO
-        return null;
+        return this;
     }
 
     private double z;
