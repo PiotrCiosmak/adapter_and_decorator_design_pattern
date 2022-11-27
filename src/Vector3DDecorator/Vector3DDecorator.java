f441ea0cb022ebf195d61684ba3baf99ac4887e6
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-11-27 20:29:12 +0100

    almost done

--- src/Vector3DDecorator/Vector3DDecorator.java
+++ src/Vector3DDecorator/Vector3DDecorator.java
@@ -6,34 +6,46 @@ import java.util.ArrayList;
 
 public class Vector3DDecorator implements IVector
 {
+    Vector3DDecorator(double x, double y, double z)
+    {
+        //TODO ustawić srcVector jakiś x i y
+        this.srcVector.setX(x);
+        this.srcVector.setY(x);
+        this.z = z;
+    }
+
     public double abs()
     {
-        //TODO
-        return 0;
+        double x = this.srcVector.getComponents().get(0);
+        double y = this.srcVector.getComponents().get(1);
+        return Math.sqrt(x * x + y * y + z * z);
     }
 
     public double cdot(IVector param)
     {
-        //TODO
-        return 0;
+        return this.srcVector.getComponents().get(0) * param.getComponents().get(0) + this.srcVector.getComponents().get(1) * param.getComponents().get(1) + z * 0;
     }
 
     public ArrayList<Double> getComponents()
     {
-        //TODO
-        return null;
+        ArrayList<Double> arrayList = new ArrayList<>();
+        arrayList.add(this.srcVector.getComponents().get(0));
+        arrayList.add(this.srcVector.getComponents().get(1));
+        arrayList.add(z);
+        return arrayList;
     }
 
-    public Vector3D(IVector param)
+    public Vector3DDecorator cross(IVector param)
     {
-        //TOOD
-        return null;
+        double newX = this.srcVector.getComponents().get(1) * 0 - z * param.getComponents().get(1);
+        double newY = -(this.srcVector.getComponents().get(0) * 0 - z * param.getComponents().get(0));
+        double newZ = this.srcVector.getComponents().get(0) * param.getComponents().get(1) - this.srcVector.getComponents().get(1) * param.getComponents().get(0);
+        return new Vector3DDecorator(newX, newY, newZ);
     }
 
     public IVector getSrcV()
     {
-        //TODO
-        return null;
+        return srcVector;
     }
 
     private IVector srcVector;
